15d32e017187420bbaa5b591c37d22f7911f15b4
monkey19920520 <1401125373@qq.com>
2017-05-19 16:43:29 +0800

    commit answer

--- main/person.js
+++
@@ -1,9 +0,0 @@
-function Person(name,age)  
-{  
-    this.name=name;  
-    this.age=age;  
-} 
-Person.prototype.introduce=function(){
-    return "My name is "+this.name+". I am "+this.age+" years old.";
-}
-module.exports = Person
