7c28b126eaa71edf9850b9cea1fd2502fbc3ed2d
monkey19920520 <1401125373@qq.com>
2017-05-19 14:25:12 +0800

    commit answer

--- main/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = function main(str) {
-   var sum=0;
-   for(var i=0;i<str.length;i++){
-     sum+=parseInt(str.substring(i,i+1));
-   }
-    console.log(sum);
-    return sum;
-};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,10 +1,15 @@
-const main = require('../main/main');
+const Person = require('../main/person');
+const Student = require('../main/student');
 
 describe('main()', () => {
 
-    it('should calculate the sum', () => {
-      expect(main("123")).toBe(6);
-      expect(main("102")).toBe(3);
+    it('should introduce person', () => {
+      let person = new Person('Tom', 21);
+      expect(person.introduce()).toBe("My name is Tom. I am 21 years old.");
     });
 
+    it('should introduce student', () => {
+      let student = new Student('Tom', 21, 2);
+      expect(student.introduce()).toBe("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
+    });
 });
