ae1b1db84f948cefabba15b6c773c3d7c731d239
monkey19920520 <1401125373@qq.com>
2017-05-21 16:56:56 +0800

    commit answer

--- main/main.js
+++ main/main.js
@@ -1,40 +1,9 @@
-const loadAllItems = require('./loadAllItems.js');
-
-module.exports = function main(inputs) {
-    var itembarcode;  
-    var item_list=[];      
-    var list='***<没钱赚商店>购物清单***\n';
-    var sum=0;
-    let item_Table = loadAllItems();
-    for(var i=0;i<inputs.length;i++){  
-        itembarcode=inputs[i];  
-        var count = 0;
-        for(var j=0;j<item_list.length;j++){  
-            if(itembarcode == item_list[j].barcode){  
-                item_list[j].count++; 
-                count++;
-            }  
-        }
-        if(count == 0){
-            for(var k = 0; k < item_Table.length; k++){
-                if(itembarcode == item_Table[k].barcode){
-                    item_Table[k].count = 1;
-                    item_list.push(item_Table[k]);
-                }
-            }
-        }
-    } 
-    for(var j = 0; j < item_list.length; j++){
-        list = list  + '名称：'+item_list[j].name+'，'+'数量：'+item_list[j].count+item_list[j].unit+'，'+'单价：'+item_list[j].price.toFixed(2)+'(元)' +'，'+'小计：'+(item_list[j].count*item_list[j].price).toFixed(2)+'(元)'+'\n';  
-        sum += item_list[j].count*item_list[j].price;
-    }
-  
-    list = list +  
-           '----------------------\n' +
-           '总计：23.00(元)\n' +
-           '**********************';
-  
-  
-    console.log(list);
-    return list;
+module.exports = function main(email, suffixes) {
+  var suffix = email.split("@");
+  var flag=false;
+  if(suffix[1]==suffixes){
+    flag=true;
+  }
+  console.log(flag)
+  return flag;
 };
