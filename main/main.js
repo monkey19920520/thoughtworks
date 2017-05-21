8369cee334e19ffecc38e64de75fc746fd64e7e4
monkey19920520 <1401125373@qq.com>
2017-05-21 17:16:51 +0800

    commit answer

--- main/main.js
+++ main/main.js
@@ -1,9 +1,71 @@
-module.exports = function main(email, suffixes) {
-  var suffix = email.split("@");
-  var flag=false;
-  if(suffix[1]==suffixes){
-    flag=true;
-  }
-  console.log(flag)
-  return flag;
+module.exports = function main(number) {
+    var numberList = [
+                   //number 0-9
+                   {
+                     up: '._.',
+                     middle: '|.|',
+                     down: '|_|',
+                   },
+                   {
+                     up: '...',
+                     middle: '..|',
+                     down: '..|',
+                   },
+                   {
+                     up: '._.',
+                     middle: '._|',
+                     down: '|_.',
+                   },
+                   {
+                     up: '._.',
+                     middle: '._|',
+                     down: '._|',
+                   },
+                   {
+                     up: '...',
+                     middle: '|_|',
+                     down: '..|',
+                   },
+                   {
+                     up: '._.',
+                     middle: '|_.',
+                     down: '._|',
+                   },
+                   {
+                     up: '._.',
+                     middle: '|_.',
+                     down: '|_|',
+                   },
+                   { //7
+                     up: '._.',
+                     middle: '..|',
+                     down: '..|',
+                   },
+                   {
+                     up: '._.',
+                     middle: '|_|',
+                     down: '|_|',
+                   },
+                   {
+                     up: '._.',
+                     middle: '|_|',
+                     down: '..|',
+                   }
+                    ];
+
+    var numbers = number.split("");
+    var first = parseInt(numbers[0]);
+    var up = numberList[first].up;
+    var middle = numberList[first].middle;
+    var down = numberList[first].down;
+ 
+    for(var i = 1; i < numbers.length; i++){
+        var index = parseInt(numbers[i]);
+        up += " "+ numberList[index].up;
+        middle += " "+ numberList[index].middle;
+        down += " "+ numberList[index].down;
+    }
+    var list  = up + "\n" + middle + "\n" + down + "\n";
+    console.log(list);
+    return list;
 };
